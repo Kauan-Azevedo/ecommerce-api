bab86b172397ea1371a36981915e179e3956d707
Kauan-Azevedo <kauanazevedo2010@gmail.com>
2024-06-14 17:38:45 -0300

    Add createModule utility function

---
+++ src/utils/create-module.ts
@@ -0,0 +1,117 @@
+import * as shell from 'shelljs';
+import * as fs from 'fs';
+import * as path from 'path';
+
+function createModule(moduleName: string): void {
+  const baseDir = path.resolve(__dirname, '..', '..', 'src', moduleName);
+  const dirs = ['controller', 'model', 'services', 'router'];
+
+  const toPascalCase = (str: string) => {
+    return str.replace(/(\w)(\w*)/g,
+      (_, g1, g2) => g1.toUpperCase() + g2.toLowerCase());
+  };
+
+  const className = toPascalCase(moduleName);
+
+  if (fs.existsSync(baseDir)) {
+    console.log(`Module ${moduleName} already exists. Aborting.`);
+    return;
+  }
+
+  shell.mkdir('-p', baseDir);
+
+  // Caminho relativo do serviço para importação no controlador
+  const servicePath = `../services/${moduleName}.service`;
+
+  dirs.forEach(dir => {
+    const dirPath = path.join(baseDir, dir);
+    if (fs.existsSync(dirPath)) {
+      console.log(`Directory ${dirPath} already exists. Skipping.`);
+      return;
+    }
+    
+    shell.mkdir('-p', dirPath);
+
+    let filePath: string;
+    let fileContent: string;
+    switch(dir) {
+      case 'controller':
+        filePath = path.join(dirPath, `${moduleName}.controller.ts`);
+        fileContent = `import { ${className}Service } from "${servicePath}";
+
+class ${className}Controller {
+
+  constructor(private readonly ${moduleName}Service: ${className}Service) { }
+
+  // Your controller methods here
+}
+
+export { ${className}Controller };`;
+        fs.writeFileSync(filePath, fileContent);
+        break;
+      case 'model':
+        filePath = path.join(dirPath, `${moduleName}.model.ts`);
+        fileContent = `import { DataTypes, Model } from "sequelize";
+import { sequelize } from "@/db/db.config";
+
+class ${className} extends Model {}
+
+${className}.init(
+  {
+    // Your model attributes here
+  },
+  {
+    sequelize,
+    modelName: "${className}",
+    paranoid: true,
+  }
+);
+
+export { ${className} };`;
+        fs.writeFileSync(filePath, fileContent);
+        break;
+      case 'services':
+        filePath = path.join(dirPath, `${moduleName}.service.ts`);
+        fileContent = `import { ${className} } from "../model/${moduleName}.model";
+
+class ${className}Service {
+  constructor() {
+    // Constructor logic here, if needed
+  }
+
+  // Your service methods here
+}
+
+export { ${className}Service };`;
+        fs.writeFileSync(filePath, fileContent);
+        break;
+      case 'router':
+        filePath = path.join(dirPath, `${moduleName}.router.ts`);
+        fileContent = `import express from "express";
+import { ${className}Controller } from "../controller/${moduleName}.controller";
+import { ${className}Service } from "../services/${moduleName}.service";
+
+const router = express.Router();
+const ${moduleName}Service = new ${className}Service(); // Instanciando o serviço
+const ${moduleName}Controller = new ${className}Controller(${moduleName}Service);
+
+// Your routes here
+
+export default router;`;
+        fs.writeFileSync(filePath, fileContent);
+        break;
+      default:
+        break;
+    }
+  });
+
+  console.log(`${moduleName} module created successfully!`);
+}
+
+const moduleName = process.argv[2];
+
+if (moduleName) {
+  createModule(moduleName);
+} else {
+  console.log('Please provide a module name');
+}
