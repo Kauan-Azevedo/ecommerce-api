6972c5b75317a1606cddbd440dab3a27e6ba11b6
Kauan-Azevedo <kauanazevedo2010@gmail.com>
2024-06-14 21:06:40 -0300

    Add Swagger documentation and update user router

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,22 @@ import { sequelize } from './db/db.config';
 import bodyParser from 'body-parser';
 import cors from "cors";
 import morgan from 'morgan';
-// import usersRouter from './users/router/users.router';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import usersRotuer from './user/router/user.router';
 
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Ecommerce API',
+            version: '1.0.0',
+            description: 'Lorem ipsum',
+        }
+    },
+    apis: ['./src/**/*.controller.ts'],
+}
+const specs = swaggerJsdoc(options);
 const app = express();
 const port = 3000;
 
@@ -12,9 +26,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan('dev'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 // Routers
-// app.use('/users', usersRouter);
+app.use('/users', usersRotuer);
 
 
 // Rota inicial
