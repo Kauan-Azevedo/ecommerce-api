f56f0ac00d8cb3ca79f9bf932760cb328feab615
Kauan-Azevedo <kauanazevedo2010@gmail.com>
2024-06-21 17:33:40 -0300

    feat: prisma seed

---
+++ prisma/seed.ts
@@ -0,0 +1,52 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const adminPermission = await prisma.permission.create({
+    data: {
+      name: "Admin",
+    },
+  });
+
+  const userPermission = await prisma.permission.create({
+    data: {
+      name: "Default",
+    },
+  });
+
+  const hashedAdminPassword = await bcrypt.hash("123321", 10);
+  await prisma.user.create({
+    data: {
+      id_permission: adminPermission.id,
+      first_name: "Dummy",
+      last_name: "Admin",
+      email: "dummyAdmin@gmail.com",
+      phone_number: "123456789",
+      password: hashedAdminPassword,
+    },
+  });
+
+  await prisma.user.create({
+    data: {
+      id_permission: userPermission.id,
+      first_name: "Dummy",
+      last_name: "User",
+      email: "dummyUser@gmail.com",
+      phone_number: "987654321",
+      password: hashedAdminPassword,
+    },
+  });
+}
+
+main()
+  .then(() => {
+    console.log("Seeds Added!");
+  })
+  .catch((e) => {
+    console.log(e);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
