31aa82a2c7501dd9df40ebe58b556c80599c190e
Kauan-Azevedo <kauanazevedo2010@gmail.com>
2024-06-27 18:47:01 -0300

    refactor: auth service

--- prisma/middleware/errorHandler.ts
+++ prisma/middleware/errorHandler.ts
@@ -9,12 +9,30 @@ const prismaErrorHandler = (
 ) => {
   if (err instanceof Prisma.PrismaClientKnownRequestError) {
     switch (err.code) {
+      case "P2000":
+        return res.status(400).json({
+          error: {
+            message: "Invalid 'where' or 'orderBy' value(s) provided.",
+          },
+        });
+      case "P2001":
+        return res.status(400).json({
+          error: {
+            message: "Invalid 'data' value(s) provided.",
+          },
+        });
       case "P2002":
         return res.status(409).json({
           error: {
             message:
               "Unique constraint failed on the field(s): " +
               (err.meta?.target ?? ""),
+          },
+        });
+      case "P2003":
+        return res.status(404).json({
+          error: {
+            message: "Record not found for the given query.",
           },
         });
       default:
