9c5a3b6d9f436579cbc54ebbf6059d139ee21b23
Kauan-Azevedo <kauanazevedo2010@gmail.com>
2024-06-27 18:52:52 -0300

    feat: auth response interface

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -3,39 +3,36 @@ import { User } from "@prisma/client";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-class AuthService {
-  constructor() {
-  }
+interface AuthResponse {
+  user: User & { permission: { name: string } };
+  token: string;
+}
 
-  async login(email: string, password: string) {
-    try {
-      const user = await prisma.user.findUnique({
-        where: { email },
-        include: {
-          permission: {
-            select: {
-              name: true,
-            },
+class AuthService {
+  async login(email: string, password: string): Promise<AuthResponse> {
+    const user = await prisma.user.findUnique({
+      where: { email },
+      include: {
+        permission: {
+          select: {
+            name: true,
           },
         },
-      });
-  
-      if (!user || !(await bcrypt.compare(password, user.password))) {
-        throw new Error("Invalid credentials!");
-      }
-  
-      const secretKey = process.env.SECRET_KEY || "";
-      const token = jwt.sign(
-        { id: user.id, email: user.email, permission: user.permission.name },
-        secretKey,
-        {
-          expiresIn: "24h",
-        },
-      );
-      return { user, token };
-    } catch(error) {
-      
+      },
+    });
+
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      throw new Error("Invalid credentials!");
     }
+
+    const secretKey = process.env.SECRET_KEY || "";
+    const token = jwt.sign(
+      { id: user.id, email: user.email, permission: user.permission.name },
+      secretKey,
+      { expiresIn: "24h" },
+    );
+
+    return { user, token };
   }
 }
 
