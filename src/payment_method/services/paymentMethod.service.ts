822a30d578a08bad62246e0e4e009d4e4728d67f
Gustavo <gustavoleandrogorges@gmail.com>
2024-07-02 14:24:24 -0300

    refactor: remove try catch on service

--- src/payment_method/services/paymentMethod.service.ts
+++ src/payment_method/services/paymentMethod.service.ts
@@ -3,60 +3,22 @@ import { PaymentMethod } from ".prisma/client"
 
 export class PaymentMethodService {
     async createPaymentMethod(paymentMethod: PaymentMethod) {
-        try {
-            const record = await prisma.paymentMethod.findFirst({ where: { name: paymentMethod.name } })
-
-            if (record) {
-                throw new Error(`Payment method with name ${paymentMethod.name} already exists.`);
-            }
-
-            return await prisma.paymentMethod.create({ data: paymentMethod })
-        } catch (error) {
-            console.error("\n\n" + error + "\n\n")
-        }
+        return await prisma.paymentMethod.create({ data: paymentMethod })
     }
 
     async getPaymentMethodById(id: number) {
-        try {
-            return await prisma.paymentMethod.findUnique({ where: { id } })
-        } catch (error) {
-            console.error("\n\n" + error + "\n\n")
-        }
+        return await prisma.paymentMethod.findUnique({ where: { id } })
     }
 
     async getAllPaymentMethods() {
-        try {
-            return await prisma.paymentMethod.findMany()
-        } catch (error) {
-            console.error("\n\n" + error + "\n\n")
-        }
+        return await prisma.paymentMethod.findMany()
     }
 
     async updatePaymentMethod(id: number, paymentMethod: PaymentMethod) {
-        try {
-            const record = await prisma.paymentMethod.update({ where: { id }, data: paymentMethod })
-
-            if (!record) {
-                throw new Error(`Payment method with ID ${id} not found on update.`);
-            }
-
-            return record
-        } catch (error) {
-            console.error("\n\n" + error + "\n\n")
-        }
+        return await prisma.paymentMethod.update({ where: { id }, data: paymentMethod })
     }
 
     async deletePaymentMethod(id: number) {
-        try {
-            const record = await prisma.paymentMethod.findUnique({ where: { id } });
-
-            if (!record) {
-                throw new Error(`Payment method with ID ${id} not found.`);
-            }
-
-            return await prisma.paymentMethod.delete({ where: { id } });
-        } catch (error) {
-            console.error("\n\n" + error + "\n\n")
-        }
+        return await prisma.paymentMethod.delete({ where: { id } });
     }
 }
