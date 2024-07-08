1f8e732266755f42dc3182ea46d7e65ffa8ad9ae
Gustavo <gustavoleandrogorges@gmail.com>
2024-07-08 00:27:17 -0300

    feat: product test

--- src/__tests__/product.spec.ts
+++ src/__tests__/product.spec.ts
@@ -1,75 +1,192 @@
-import { Request, Response } from "express";
-import { ProductService } from "../product/services/product.service";
-import { ProductController } from "../product/controller/product.controller";
+import request from 'supertest';
+import { app, server } from '@/main';
+import { PrismaClient } from '@prisma/client';
+import { exec } from 'child_process';
+import util from 'util';
+const execPromise = util.promisify(exec);
 
-describe("ProductController", () => {
-  let productService: ProductService;
-  let productController: ProductController;
-  let req: Request;
-  let res: Response;
+const prisma = new PrismaClient();
 
-  beforeEach(() => {
-    productService = new ProductService();
-    productController = new ProductController(productService);
-    req = {} as Request;
-    res = {} as Response;
-    res.status = jest.fn().mockReturnThis();
-    res.json = jest.fn();
-    res.send = jest.fn();
+describe("product API", () => {
+  beforeEach(async () => {
+    process.env.APPLICATION_DEV_MODE = "false"
+    // Reset the database state before each test
+    try {
+      await execPromise('npx prisma migrate reset --force');
+      // Add any other necessary cleanup or seeding here
+    } catch (error) {
+      console.error('Failed to reset database:', error);
+    }
   });
 
-  describe("getAll", () => {
-    it("should return all products", async () => {
-      const products = [
-        { id: 1, name: "Product 1" },
-        { id: 2, name: "Product 2" },
-      ];
-      productService.getAll = jest.fn().mockResolvedValue(products);
+  afterAll(async () => {
+    await prisma.$disconnect();
+    server.close();
+  });
 
-      await productController.getAll(req, res);
+  it("should get 404 when getting a product by id that don't exits", async () => {
 
-      expect(res.status).toHaveBeenCalledWith(200);
-      expect(res.json).toHaveBeenCalledWith(products);
-    });
+    const response = await request(app)
+      .get("/products/1")
+      .expect(404);
 
-    it("should handle errors and return 500 status code", async () => {
-      const errorMessage = "Internal server error";
-      productService.getAll = jest
-        .fn()
-        .mockRejectedValue(new Error(errorMessage));
-
-      await productController.getAll(req, res);
-
-      expect(res.status).toHaveBeenCalledWith(500);
-      expect(res.json).toHaveBeenCalledWith({ message: errorMessage });
+    expect(response.body).toEqual({
+      error: "Product not found"
     });
   });
 
-  describe("getById", () => {
-    it("should return a product by id", async () => {
-      const id = 1;
-      const product = { id, name: "Product 1" };
-      productService.getById = jest.fn().mockResolvedValue(product);
-      req.params = { id: id.toString() };
+  it("should get 404 when getting all products with no product created", async () => {
 
-      await productController.getById(req, res);
+    const response = await request(app)
+      .get("/products")
+      .expect(404);
 
-      expect(res.status).toHaveBeenCalledWith(200);
-      expect(res.json).toHaveBeenCalledWith(product);
+    expect(response.body).toEqual({
+      error: "No products found"
     });
+  })
 
-    it("should handle errors and return 500 status code", async () => {
-      const id = 1;
-      const errorMessage = "Internal server error";
-      productService.getById = jest
-        .fn()
-        .mockRejectedValue(new Error(errorMessage));
-      req.params = { id: id.toString() };
+  it("should return 400 when inserting a product with a negative value", async () => {
+    const product = {
+      name: "Product 1",
+      value: -10,
+      stock: 10
+    }
 
-      await productController.getById(req, res);
+    const response = await request(app)
+      .post("/products/create")
+      .send(product)
+      .expect(400);
 
-      expect(res.status).toHaveBeenCalledWith(500);
-      expect(res.json).toHaveBeenCalledWith({ message: errorMessage });
+    expect(response.body).toEqual({
+      error: "invalid payload provided."
     });
+  })
+
+  it("should return 400 when inserting a product with a negative stock", async () => {
+    const product = {
+      name: "Product 1",
+      value: 10,
+      stock: -10
+    }
+
+    const response = await request(app)
+      .post("/products/create")
+      .send(product)
+      .expect(400);
+
+    expect(response.body).toEqual({
+      error: "invalid payload provided."
+    });
+  })
+
+  it("should return 400 when creating a product with an empty name", async () => {
+    const product = {
+      name: "",
+      value: 10,
+      stock: 10
+    }
+
+    const response = await request(app)
+      .post("/products/create")
+      .send(product)
+      .expect(400);
+
+    expect(response.body).toEqual({
+      error: "invalid payload provided."
+    });
+  })
+})
+
+it("should return 404 when updating a product that don't exits", async () => {
+  const product = {
+    name: "Product 1",
+    value: 10,
+    stock: 10
+  }
+
+  const response = await request(app)
+    .put("/products/1")
+    .send(product)
+    .expect(404);
+
+  expect(response.body).toEqual({
+    error: "Product not found"
   });
-});
+})
+
+it("should return 404 when deleting a product that don't exits", async () => {
+  const response = await request(app)
+    .delete("/products/1")
+    .expect(404);
+
+  expect(response.body).toEqual({
+    error: "Product not found"
+  });
+})
+
+it("should create a product", async () => {
+  const product = {
+    name: "Product 1",
+    value: 10,
+    stock: 10
+  }
+
+  const response = await request(app)
+    .post("/products/create")
+    .send(product)
+    .expect(201);
+
+  expect(response.body.id).toEqual(1);
+  expect(response.body.name).toEqual("Product 1");
+  expect(response.body.value).toEqual(10);
+  expect(response.body.stock).toEqual(10);
+})
+
+it("should update a product", async () => {
+  const product = {
+    name: "Product 1",
+    value: 10,
+    stock: 10
+  }
+
+  await request(app)
+    .post("/products/create")
+    .send(product)
+    .expect(201);
+
+  const updatedProduct = {
+    name: "Product 2",
+    value: 20,
+    stock: 20
+  }
+
+  const response = await request(app)
+    .put("/products/1")
+    .send(updatedProduct)
+    .expect(200);
+
+  expect(response.body.id).toEqual(1);
+  expect(response.body.name).toEqual("Product 2");
+  expect(response.body.value).toEqual(20);
+  expect(response.body.stock).toEqual(20);
+})
+
+it("should delete a product", async () => {
+  const product = {
+    name: "Product 1",
+    value: 10,
+    stock: 10
+  }
+
+  await request(app)
+    .post("/products/create")
+    .send(product)
+    .expect(201);
+
+  const response = await request(app)
+    .delete("/products/1")
+    .expect(200);
+
+  expect(response.body).toEqual({});
+})
